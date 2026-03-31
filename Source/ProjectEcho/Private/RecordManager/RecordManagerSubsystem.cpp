9f20d0f84489622add90b2b8bb4ba527cb773d4c
Volodia <volodiabussereau@gmail.com>
2026-03-31 15:26:15 +0200

    [GP][ADD] GlobalTimeline struct Start

--- Source/ProjectEcho/Private/RecordManager/RecordManagerSubsystem.cpp
+++ Source/ProjectEcho/Private/RecordManager/RecordManagerSubsystem.cpp
@@ -1,7 +1,135 @@
 ﻿#include <RecordManager/RecordManagerSubsystem.h>
 
+#include "Developer/AITestSuite/Public/AITestsCommon.h"
+#include "Engine/World.h"
+
+#pragma region Timeline
+
+const FRecordTransformKey* FTimeline::GetNextTransformKey(const float& TimeKey) const
+{
+	for (const FRecordTransformKey& TransformKey : TransformKeys)
+	{
+		if (TransformKey.TimeKey >= TimeKey)
+		{
+			return &TransformKey;
+		}
+	}
+	return nullptr;
+}
+
+const FRecordTransformKey* FTimeline::GetPreviousTransformKey(const float& TimeKey) const
+{
+	const FRecordTransformKey* key = nullptr;
+	for (const FRecordTransformKey& TransformKey : TransformKeys)
+	{
+		if (TransformKey.TimeKey <= TimeKey)
+		{
+			key = &TransformKey;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return key;
+}
+
+const float& FTimeline::GetLastTimeKey()
+{
+	return TransformKeys[TransformKeys.Num() - 1].TimeKey;
+}
+
+void FTimeline::RegisterEchoActor(AActor* InEchoActor)
+{
+	EchoActor = InEchoActor;
+}
+
+void FTimeline::RecordTransformKey(AActor* RecordedActor, const float& CurrentTimeKey)
+{
+	//Create and Init values of TransformKey
+	FRecordTransformKey TransformKey;
+	TransformKey.TimeKey = CurrentTimeKey;
+	TransformKey.Position = RecordedActor->GetActorLocation();
+	TransformKey.Rotation = RecordedActor->GetActorRotation();
+	TransformKey.Scale = RecordedActor->GetActorScale();
+	
+	//Add it to the list and Sort the list (list has to be in order)
+	TransformKeys.Add(TransformKey);
+	TransformKeys.Sort([](const FRecordTransformKey& A, const FRecordTransformKey& B)
+	{
+		return A.TimeKey < B.TimeKey;
+	});
+}
+
+void FTimeline::PlayReplay(const float& PreviousKey,const float& CurrentTimeKey, bool bIsInRewind)
+{
+	if (!IsValid(EchoActor)) return;
+	if (GetLastTimeKey() < CurrentTimeKey) return;
+	
+	//Play Transform Key (Lerp between two closest Keys)
+	const FRecordTransformKey* PreviousTransformKey = GetPreviousTransformKey(CurrentTimeKey);
+	const FRecordTransformKey* NextTransformKey = GetNextTransformKey(CurrentTimeKey);
+	
+	//Place Actor according to previous and next TransformKey 
+	float lerpValue = (CurrentTimeKey - PreviousTransformKey->TimeKey) / (NextTransformKey->TimeKey - PreviousTransformKey->TimeKey);
+	EchoActor->SetActorLocation(FMath::Lerp(PreviousTransformKey->Position, NextTransformKey->Position, lerpValue));
+	EchoActor->SetActorRotation(FMath::Lerp(PreviousTransformKey->Rotation, NextTransformKey->Rotation, lerpValue));
+	EchoActor->SetActorScale3D(FMath::Lerp(PreviousTransformKey->Scale, NextTransformKey->Scale, lerpValue));
+}
+
+void FTimeline::ActivateTimeline(bool bInIsActive)
+{
+	if (IsValid(EchoActor))
+	{
+		bIsActive = bInIsActive;
+		EchoActor->SetActorHiddenInGame(bInIsActive);
+	}
+}
+
+void FGlobalTimeline::Play(const float& PreviousTimeKey, const float& CurrentTimeKey, bool bIsInRewind, bool& bOutHasReachedEnd)
+{
+	//TODO: Add Rewind behavior
+	
+	bool bHasActiveTimeline = false;
+	
+	for (FTimeline& Timeline : Timelines)
+	{
+		// Activate or deactivate Timeline
+		float LocalTimeKey = CurrentTimeKey - Timeline.StartTimeKey();
+		if (Timeline.bIsActive)
+		{
+			if (Timeline.GetLastTimeKey() < LocalTimeKey)
+			{
+				Timeline.ActivateTimeline(false);
+				continue;
+			}
+		}
+		else if (LocalTimeKey > 0 && LocalTimeKey < Timeline.GetLastTimeKey())
+		{
+			Timeline.ActivateTimeline(true);
+		}
+		else
+		{
+			continue;
+		}
+		
+		// Play Timeline
+		bHasActiveTimeline = true;
+		Timeline.PlayReplay(PreviousTimeKey, CurrentTimeKey, bIsInRewind);
+	}
+	
+	bOutHasReachedEnd = !bHasActiveTimeline;
+}
+
+bool FGlobalTimeline::HasAvailableTimelineSlot()
+{
+	return Timelines.Num() < MaxSlots;
+}
+
 // Fill out your copyright notice in the Description page of Project Settings.
 void URecordManagerSubsystem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
+
+#pragma endregion
--- Source/ProjectEcho/Public/RecordManager/RecordManagerSubsystem.h
+++ Source/ProjectEcho/Public/RecordManager/RecordManagerSubsystem.h
@@ -3,6 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "UObject/ObjectPtr.h"
 
 #include "RecordManagerSubsystem.generated.h"
 
@@ -36,17 +39,20 @@ struct FTimeline
 	
 	//TimeKey of Start of Timeline (from Global Timeline)
 	float StartTimeKey;
+	
+	//Whether Timeline is currently active and showing a Replay (In Rewind or not)
+	bool bIsActive = false;
 
 	//Get Closest Next Key
 	UFUNCTION(BlueprintCallable)
-	const FRecordTransformKey* GetNextTransformKey(const float* TimeKey) const;
+	const FRecordTransformKey* GetNextTransformKey(const float& TimeKey) const;
 	
 	//Get Closest Previous Key
 	UFUNCTION(BlueprintCallable)
-	const FRecordTransformKey* GetPreviousTransformKey(const float* TimeKey) const;
+	const FRecordTransformKey* GetPreviousTransformKey(const float& TimeKey) const;
 	
 	//Get Last Key of Timeline (End of Timeline, in Local Time)
-	const float* GetLastTimeKey();
+	const float& GetLastTimeKey();
 	
 	//Save Echo Actor for Replays
 	UFUNCTION(BlueprintCallable)
@@ -54,7 +60,7 @@ struct FTimeline
 	
 	//Record current Transform into TransformKey in Timeline List
 	UFUNCTION(BlueprintCallable)
-	void RecordTransformKey(AActor* RecordedActor, const float* CurrentTimeKey);
+	void RecordTransformKey(AActor* RecordedActor, const float& CurrentTimeKey);
 	
 	/* Replay Function
 	 * Play Current Frame of the Replay with given PreviousKey Played and CurrentTimeKey 
@@ -62,79 +68,27 @@ struct FTimeline
 	 * - Calculate the Transform of the Actor based on last and next TransformKey
 	 */
 	UFUNCTION(BlueprintCallable)
-	void PlayReplay(const float* PreviousKey,const float* CurrentTimeKey, bool bIsInRewind) -> void;
+	void PlayReplay(const float& PreviousKey,const float& CurrentTimeKey, bool bIsInRewind) -> void;
+	
+	UFUNCTION(BlueprintCallable)
+	void ActivateTimeline(bool bInIsActive);
 };
 
-inline const FRecordTransformKey* FTimeline::GetNextTransformKey(const float* TimeKey) const
+USTRUCT(Blueprintable)
+struct FGlobalTimeline
 {
-	for (const FRecordTransformKey& TransformKey : TransformKeys)
-	{
-		if (TransformKey.TimeKey >= *TimeKey)
-		{
-			return &TransformKey;
-		}
-	}
-	return nullptr;
-}
-
-inline const FRecordTransformKey* FTimeline::GetPreviousTransformKey(const float* TimeKey) const
-{
-	const FRecordTransformKey* key = nullptr;
-	for (const FRecordTransformKey& TransformKey : TransformKeys)
-	{
-		if (TransformKey.TimeKey <= *TimeKey)
-		{
-			key = &TransformKey;
-		}
-		else
-		{
-			break;
-		}
-	}
-	return key;
-}
-
-inline const float* FTimeline::GetLastTimeKey()
-{
-	return &TransformKeys[TransformKeys.Num() - 1].TimeKey;
-}
-
-inline void FTimeline::RegisterEchoActor(AActor* InEchoActor)
-{
-	EchoActor = InEchoActor;
-}
-
-inline void FTimeline::RecordTransformKey(AActor* RecordedActor, const float* CurrentTimeKey)
-{
-	//Create and Init values of TransformKey
-	FRecordTransformKey TransformKey;
-	TransformKey.TimeKey = *CurrentTimeKey;
-	TransformKey.Position = RecordedActor->GetActorLocation();
-	TransformKey.Rotation = RecordedActor->GetActorRotation();
-	TransformKey.Scale = RecordedActor->GetActorScale();
+	GENERATED_BODY()
 	
-	//Add it to the list and Sort the list (list has to be in order)
-	TransformKeys.Add(TransformKey);
-	TransformKeys.Sort([](const FRecordTransformKey& A, const FRecordTransformKey& B)
-	{
-		return A.TimeKey < B.TimeKey;
-	});
-}
-
-inline void FTimeline::PlayReplay(const float* PreviousKey,const float* CurrentTimeKey, bool bIsInRewind)
-{
-	if (GetLastTimeKey() < CurrentTimeKey) return;
+	TArray<FTimeline> Timelines;
 	
-	//Play Transform Key (Lerp between two closest Keys)
-	const FRecordTransformKey* PreviousTransformKey = GetPreviousTransformKey(CurrentTimeKey);
-	const FRecordTransformKey* NextTransformKey = GetNextTransformKey(CurrentTimeKey);
+	int MaxSlots = 5;
 	
-	//Place Actor according to previous and next TransformKey 
-	float lerpValue = (*CurrentTimeKey - PreviousTransformKey->TimeKey) / (NextTransformKey->TimeKey - PreviousTransformKey->TimeKey);
-	EchoActor->SetActorLocation(FMath::Lerp(PreviousTransformKey->Position, NextTransformKey->Position, lerpValue));
-	EchoActor->SetActorRotation(FMath::Lerp(PreviousTransformKey->Rotation, NextTransformKey->Rotation, lerpValue));
-	EchoActor->SetActorScale3D(FMath::Lerp(PreviousTransformKey->Scale, NextTransformKey->Scale, lerpValue));
-}
+	//Play CurrentFrame for all Active Timelines, activate timelines that have not yet been activated
+	void Play(const float& PreviousTimeKey, const float& CurrentTimeKey, bool bIsInRewind, bool& bOutHasReachedEnd);
+	
+	//Whether GlobalTimeline has an available slot to start recording in it
+	bool HasAvailableTimelineSlot();
+};
 
 #pragma endregion
 
@@ -144,5 +98,9 @@ class PROJECTECHO_API URecordManagerSubsystem : public UTickableWorldSubsystem
 	GENERATED_BODY()
 	
 	virtual void Tick(float DeltaTime) override;
+protected:
+	FGlobalTimeline GlobalTimeline;
+	
+private:
 };
 
