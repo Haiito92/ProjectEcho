82799a5e78a321cf673a4fcac30af20ac822eb77
Haiito92 <hannaantoinebusiness@gmail.com>
2026-04-01 18:50:12 +0200

    [GP][ADD] CheatManager class

---
+++ Source/ProjectEcho/Private/Cheats/EchoCheatManager.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Cheats/EchoCheatManager.h"
+
+#include "EchoSystem.h"
+#include "Tools/Debug/EchoDebug.h"
+#include "Tools/Debug/MessageType.h"
+
+void UEchoCheatManager::ToggleSystemDebug()
+{
+	UEchoDebug::AddOnScreenDebugMessage(
+		EEchoSystem::Record,
+		EMessageType::Log,
+		"Toggle Record Debug",
+		FColor::Magenta,
+		5.0f);
+}
---
+++ Source/ProjectEcho/Public/Cheats/EchoCheatManager.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CheatManager.h"
+#include "EchoCheatManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTECHO_API UEchoCheatManager : public UCheatManager
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(Exec)
+	void ToggleSystemDebug();
+};
