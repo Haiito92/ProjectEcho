7241649e2342d90d3b5e2ec058dee0bec3678b56
Haiito92 <hannaantoinebusiness@gmail.com>
2026-04-09 14:50:13 +0200

    [GP][ADD] Echo GameMode

---
+++ Source/ProjectEcho/Private/GameModes/EchoGameMode.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameModes/EchoGameMode.h"
+
+void AEchoGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitializeGame();
+}
+
+void AEchoGameMode::InitializeGame()
+{
+	ReceiveInitializeGame();
+}
+
+void AEchoGameMode::StartGame()
+{
+	ReceiveStartGame();
+}
+
+void AEchoGameMode::EndGame()
+{
+	ReceiveEndGame();
+}
---
+++ Source/ProjectEcho/Public/GameModes/EchoGameMode.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EchoGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class PROJECTECHO_API AEchoGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+	virtual void BeginPlay() override;
+	
+	void InitializeGame();
+	void StartGame();
+	void EndGame();
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent)
+	void ReceiveInitializeGame();
+	
+	UFUNCTION(BlueprintImplementableEvent)
+	void ReceiveStartGame();
+	
+	UFUNCTION(BlueprintImplementableEvent)
+	void ReceiveEndGame();
+};
