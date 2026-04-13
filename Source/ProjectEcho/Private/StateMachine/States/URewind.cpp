68af2f9fef452a4426bf16428cf5171c0c41ec15
LucLenne <luc.lenne@outlook.fr>
2026-04-13 11:52:48 +0200

    [GP][ADD] Create class rewind

---
+++ Source/ProjectEcho/Private/StateMachine/States/URewind.cpp
@@ -0,0 +1,20 @@
+#include "StateMachine/States/URewind.h"
+#include "RecordManager/EchoActor.h"
+#include "RecordManager/RecordHandlerComponent.h"
+#include "StateMachine/ACharacterST.h"
+
+void URewind::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+
+void URewind::Enter()
+{
+	Super::Enter();	
+}
+
+void URewind::Exit()
+{
+	Super::Exit();
+}
