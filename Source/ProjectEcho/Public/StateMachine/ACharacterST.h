a38ff87ab38d873e8f8be3a525039c35d3896693
James <63363788+Frynity-James@users.noreply.github.com>
2026-04-16 13:37:50 +0200

    [GP][ADD] Cube dies in death zone

--- Source/ProjectEcho/Public/StateMachine/ACharacterST.h
+++ Source/ProjectEcho/Public/StateMachine/ACharacterST.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "KillMechanic/Killable.h"
 #include "ACharacterST.generated.h"
 
 class UInputComponent;
@@ -14,7 +15,7 @@ class UStateMachine;
 class UEnhancedInputLocalPlayerSubsystem;
 
 UCLASS()
-class PROJECTECHO_API ACharacterST : public ACharacter
+class PROJECTECHO_API ACharacterST : public ACharacter, public IKillable
 {
 	GENERATED_BODY()
 
@@ -76,8 +77,8 @@ public:
 	void PlayerTakeDamage(int value);
 	
 	UFUNCTION(BlueprintCallable)
-	void Kill();
-
+	virtual void Kill_Implementation() override;
+	
 	UFUNCTION(BlueprintCallable)
 	void DeathEnd();
 	
