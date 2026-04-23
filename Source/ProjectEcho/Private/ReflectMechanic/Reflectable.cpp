58fde150064695223cbf667c4b5cf3c1a1c85ee2
Haiito92 <hannaantoinebusiness@gmail.com>
2026-04-23 09:50:25 +0200

    [GP][ADD] Reflect and Propulse Mechanic base classes (no implementation)

---
+++ Source/ProjectEcho/Private/ReflectMechanic/Reflectable.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ReflectMechanic/Reflectable.h"
+
+
+// Add default functionality here for any IReflactable functions that are not pure virtual.
---
+++ Source/ProjectEcho/Public/PropulseMechanic/PropulseComponent.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PropulseComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), Blueprintable, BlueprintType, meta=(BlueprintSpawnableComponent))
+class PROJECTECHO_API UPropulseComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UPropulseComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
