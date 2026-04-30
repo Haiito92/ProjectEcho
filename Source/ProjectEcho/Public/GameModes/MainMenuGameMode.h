d6bcb3ead6249a4d9974dcec6570a3e3e74aaaf7
Haiito92 <hannaantoinebusiness@gmail.com>
2026-04-30 15:41:10 +0200

    [GP][ADD] Main Menu Level

---
+++ Source/ProjectEcho/Public/GameModes/MainMenuGameMode.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EchoGameModeBase.h"
+#include "MainMenuGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTECHO_API AMainMenuGameMode : public AEchoGameModeBase
+{
+	GENERATED_BODY()
+};
---
+++ Source/ProjectEcho/Public/HUDs/MainMenuHUD.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EchoHUDBase.h"
+#include "MainMenuHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTECHO_API AMainMenuHUD : public AEchoHUDBase
+{
+	GENERATED_BODY()
+};
