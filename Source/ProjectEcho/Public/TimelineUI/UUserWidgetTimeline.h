2c8e04e775914f2acd6bb890560674d3e045e2d2
LucLenne <luc.lenne@outlook.fr>
2026-05-05 11:30:33 +0200

    [GP][MOD] Debug timeline

--- Source/ProjectEcho/Public/TimelineUI/UUserWidgetTimeline.h
+++ Source/ProjectEcho/Public/TimelineUI/UUserWidgetTimeline.h
@@ -5,27 +5,11 @@
 #include "RecordManager/RecordManagerSubsystem.h"
 #include "UUserWidgetTimeline.generated.h"
 
-USTRUCT(BlueprintType)
-struct FTimelineData
-{
-	GENERATED_BODY()
-	UPROPERTY(BlueprintReadWrite,EditAnywhere)
-	float StartTime;
-	UPROPERTY(BlueprintReadWrite,EditAnywhere)
-	float EndTime;
-	UPROPERTY(BlueprintReadWrite,EditAnywhere)
-	FColor Color;
-};
 
 UCLASS(BlueprintType)
 class PROJECTECHO_API UUserWidgetTimeline : public UUserWidget
 {
 	GENERATED_BODY()
-public:
-	UPROPERTY(BlueprintReadWrite,EditAnywhere)
-	TArray<FTimelineData> TimelineUI;
-private:
-	URecordManagerSubsystem* RecordManagerSubsystem;
 };
 	
 	
