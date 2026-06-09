91a7231ed7ac5646fbd0b5817b30a73ac8274ea8
Antoine Hanna <hannaantoinebusiness@gmail.com>
2026-06-09 11:18:19 +0200

    [GP][FIX] Focus loss in world UI

---
+++ Source/ProjectEcho/Private/GameInstance/EchoGameplayStatics.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameInstance/EchoGameplayStatics.h"
+
+#include "Viewport/EchoGameViewportClient.h"
+
+UEchoGameViewportClient* UEchoGameplayStatics::GetEchoGameViewportClient(const UObject* WorldContextObject)
+{
+	return Cast<UEchoGameViewportClient>(WorldContextObject->GetWorld()->GetGameViewport());
+}
