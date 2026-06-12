6568cdabc0c71f1ca69252ccf738e7182ff3652f
Antoine Hanna <hannaantoinebusiness@gmail.com>
2026-06-12 17:04:54 +0200

    [GP][ADD] Start of dialogue system (in 2D)

---
+++ Source/ProjectEcho/Private/Audio/Dialogues/DialogueWorldSubsystem.cpp
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Audio/Dialogues/DialogueWorldSubsystem.h"
+
+#include "EchoSystem.h"
+#include "Kismet/GameplayStatics.h"
+#include "Tools/Debug/EchoDebug.h"
+#include "Tools/Debug/EchoMessageType.h"
+
+void UDialogueWorldSubsystem::InitializeDialogueSubsystem()
+{
+}
+
+void UDialogueWorldSubsystem::QueueDialogue2D(USoundBase* DialogueToQueue)
+{
+	if (!IsValid(DialogueToQueue))
+	{
+		UEchoDebug::LogAndAddOnScreenDebugMessage(EEchoSystem::Audio, EEchoMessageType::Error, "Can't queue dialogue: dialogue is not valid!", FColor::Red, 3.0f);
+		return;
+	}
+	
+	if (CurrentlyPlayedDialogue == nullptr)
+	{
+		PlayDialogue2D(DialogueToQueue);
+		return;
+	}
+	
+	QueuedDialogues.Add(DialogueToQueue);
+}
+
+void UDialogueWorldSubsystem::PlayNextDialogue2D()
+{
+	if (QueuedDialogues.IsEmpty())
+	{
+		UEchoDebug::LogAndAddOnScreenDebugMessage(EEchoSystem::Audio, EEchoMessageType::Error, "Can't play next dialogue: queue is empty!", FColor::Red, 3.0f);
+		return;
+	}
+	
+	USoundBase* NextDialogue = QueuedDialogues[0];
+	QueuedDialogues.RemoveAt(0);
+	
+	PlayDialogue2D(NextDialogue);
+}
+
+void UDialogueWorldSubsystem::PlayDialogue2D(USoundBase* DialogueToPlay)
+{
+	float DialogueDuration = DialogueToPlay->GetDuration();
+	
+	FTimerDelegate DialogueTimerDelegate;
+	DialogueTimerDelegate.BindUObject(this, &ThisClass::OnCurrentDialogueEnded);
+	
+	GetWorld()->GetTimerManager().SetTimer(
+		PlayedDialogueTimerHandle,
+		DialogueTimerDelegate,
+		DialogueDuration,
+		false);
+	
+	UGameplayStatics::PlaySound2D(GetWorld(), DialogueToPlay);
+}
+
+void UDialogueWorldSubsystem::OnCurrentDialogueEnded()
+{
+	CurrentlyPlayedDialogue = nullptr;
+	
+	PlayNextDialogue2D();
+}
---
+++ Source/ProjectEcho/Public/Audio/Dialogues/DialogueWorldSubsystem.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "DialogueWorldSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTECHO_API UDialogueWorldSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION()
+	void InitializeDialogueSubsystem();
+	
+	UFUNCTION(BlueprintCallable)
+	void QueueDialogue2D(USoundBase* DialogueToQueue);
+	
+private:
+	
+	void PlayNextDialogue2D();
+	void PlayDialogue2D(USoundBase* DialogueToPlay);
+	void OnCurrentDialogueEnded();
+	
+	
+	UPROPERTY()
+	USoundBase* CurrentlyPlayedDialogue;
+	UPROPERTY()
+	FTimerHandle PlayedDialogueTimerHandle;
+	
+	UPROPERTY()
+	TArray<USoundBase*> QueuedDialogues;
+};
