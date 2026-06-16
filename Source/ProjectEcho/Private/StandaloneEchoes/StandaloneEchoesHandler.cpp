8e8e8bb0d02f43cf0d5fb22c6f8b9a9f831ea18a
Volod <volodiabussereau@gmail.com>
2026-06-16 15:56:31 +0200

    [GP][MOD] Standalone Echoes Animation finished

--- Source/ProjectEcho/Private/StandaloneEchoes/StandaloneEchoesHandler.cpp
+++ Source/ProjectEcho/Private/StandaloneEchoes/StandaloneEchoesHandler.cpp
@@ -352,15 +352,6 @@ void AStandaloneEchoesHandler::RecreateAllAnimationKeys(int TimelineIndex)
 				EchoTimelines[TimelineIndex].RecordAnimationKeys.Add(FRecordAnimationKey(AnimationValueTopBody, RecordActionKey.TimeKey));
 				continue;
 			}
-			
-			if (RecordActionKey.Action.ActionEnum == ERecordedAction::Interact || RecordActionKey.Action.ActionEnum == ERecordedAction::TryGrab)
-			{
-				FRecordAnimationValue AnimationValueInteractOn = FRecordAnimationValue(EAnimationValueReference::IsInteract, true);
-				FRecordAnimationValue AnimationValueInteractOff = FRecordAnimationValue(EAnimationValueReference::IsInteract, false);
-				EchoTimelines[TimelineIndex].RecordAnimationKeys.Add(FRecordAnimationKey(AnimationValueInteractOn, RecordActionKey.TimeKey));
-				EchoTimelines[TimelineIndex].RecordAnimationKeys.Add(FRecordAnimationKey(AnimationValueInteractOn, RecordActionKey.TimeKey + 0.2));
-				continue;
-			}
 		}
 		
 		//Setup Speed Animation Keys
